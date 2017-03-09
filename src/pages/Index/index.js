c93f350e84f35c9471e5dee9a0b6d2a733a5392f
Oh Jinkyun <tintypemolly@gmail.com>
2017-03-09 17:53:20 +0900

    added gif in index page

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import indexImage from './index_image.gif';
 
 const Index = (props) => (
   <Paper style={{
     padding: 20
   }}>
-    This is the index page.
+    <div style={{maxWidth: 500}}>
+      <img src={indexImage}/>
+      <p>This is the index page.</p>
+    </div>
   </Paper>
 );
 
