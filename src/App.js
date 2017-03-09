a6bc784e1d3d689ed384bdbb6d03e59e75d09934
Oh Jinkyun <tintypemolly@gmail.com>
2017-03-09 14:57:29 +0900

    added nested menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { browserHistory } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
 
 class App extends Component {
 
@@ -26,16 +27,35 @@ class App extends Component {
           docked={false}
           onRequestChange={(open) => this.setState({drawerOpened: open})}
         >
-          <MenuItem onTouchTap={
-            () => {
-              browserHistory.push('about');
-              this.handleDrawerToggle();
+          <MenuItem
+            onTouchTap={
+              () => {
+                browserHistory.push('about');
+                this.handleDrawerToggle();
+              }
             }
-          }>
+          >
             About
           </MenuItem>
-          <MenuItem onTouchTap={this.handleDrawerToggle}>Bar</MenuItem>
-          <MenuItem onTouchTap={this.handleDrawerToggle}>Baz</MenuItem>
+          <MenuItem
+            primaryText='Bar'
+            onTouchTap={
+              () => {
+                browserHistory.push('no-such-url');
+                this.handleDrawerToggle();
+              }
+            }
+          />
+          <MenuItem
+            primaryText='Family Members'
+            rightIcon={<ArrowDropRight/>}
+            menuItems={[
+              <MenuItem onTouchTap={this.handleDrawerToggle}>Kobayashi</MenuItem>,
+              <MenuItem onTouchTap={this.handleDrawerToggle}>Thor</MenuItem>,
+              <MenuItem onTouchTap={this.handleDrawerToggle}>Quetzalcoatl</MenuItem>,
+              <MenuItem onTouchTap={this.handleDrawerToggle}>Fafnir</MenuItem>,
+            ]}
+          />
         </Drawer>
         <div style={{
           padding: 20
